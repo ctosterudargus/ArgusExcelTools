05e9e0a0d3f94e5415494ee521148fff5d96a740
ctosterudargus <christian.tosterud@argusco.com>
2025-08-05 22:34:06 -0500

    Create Cable.cs

---
+++ ArgusExcelTools/Cable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgusElectrical
+{
+    internal class Cable
+    {
+        public string ID { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Quantity { get; set; }
+        public string Size { get; set; }
+        public string Type { get; set; }
+        public string Ground { get; set; }
+        public string RacewayRouting { get; set; }
+        public string SignalType { get; set; }
+
+    }
+}
