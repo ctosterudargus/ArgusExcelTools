658e2397b53afd8dbbd17337df32cdbf210b0ddf
ctosterudargus <christian.tosterud@argusco.com>
2025-08-05 22:35:07 -0500

    Create Raceway.cs

---
+++ ArgusExcelTools/Raceway.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgusElectrical
+{
+    internal class Raceway
+    {
+        public string ID { get; set; }
+
+        public string Size { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string CircuitType { get; set; }
+
+        public string CableFill { get; set; }
+
+        public string DuctbankRouting { get; set; }
+
+    }
+}
